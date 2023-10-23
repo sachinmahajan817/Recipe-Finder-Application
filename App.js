04e95361a76cc5ba6efd6c8b5018b4136066afcb
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2023-10-23 23:29:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,110 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Favourites from "./components/Favourites";
+import RecipeItem from "./components/RecipeItem";
+import NotFound from "./components/NotFound";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [savedItems, setSavedItems] = useState(() => {
+    const localData = localStorage.getItem("recipes");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  const inputField = useRef(null);
+  const navigate = useNavigate();
+
+  // search handler
+  const searchHander = (e) => {
+    e.preventDefault();
+
+    getData(searchQuery);
+
+    setSearchQuery("");
+    inputField.current.blur();
+    setRecipes([]);
+  };
+
+  const getData = async (searchQuery) => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchQuery}`
+      );
+      if (!res.ok) throw new Error("Something went wrong!");
+      const data = await res.json();
+      if (data.result === 0) throw new Error("No recipe found!");
+      setRecipes(data?.data?.recipes);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const checkLocadData = (data) => {
+    const localData = JSON.parse(localStorage.getItem("recipes"));
+    const existedData = localData?.some((item) => item.id === data.id);
+
+    if (!existedData) {
+      setSavedItems([...savedItems, data]);
+    } else {
+      const filteredData = localData.filter((item) => item.id !== data.id);
+      setSavedItems(filteredData);
+    }
+  };
+
+  const favouriteHandler = (id) => {
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
+      .then((res) => res.json())
+      .then((data) => checkLocadData(data.data.recipe));
+
+    navigate("/favourites");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(savedItems));
+  }, [savedItems]);
+
+  return (
+    <>
+      <div className="App min-h-screen bg-rose-50 text-lg text-gray-600">
+        <Navbar
+          savedItems={savedItems}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          inputField={inputField}
+          searchHander={searchHander}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home recipes={recipes} loading={loading} error={error} />}
+          />
+          <Route
+            path="/favourites"
+            element={<Favourites savedItems={savedItems} />}
+          />
+          <Route
+            path="/recipe-item/:id"
+            element={
+              <RecipeItem
+                favouriteHandler={favouriteHandler}
+                savedItems={savedItems}
+              />
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Recipe-Finder-Application
-Using Javascript, CSS and HTML
+# RecipeFinder-Application
+
+Hosted Website Link :- https://calm-pothos-3fd0d5.netlify.app/
+
+RecipeFinder is an online recipe application that brings culinary inspiration to your fingertips. 
+Discover a world of delicious recipes, beautifully presented, and ready to inspire your next meal. 
+This project showcases the power of React, React Router, Tailwind CSS, data fetching, and CSS animations to create an immersive and visually appealing cooking experience.
+
+Features:- 
+
+Recipe Search: Explore a vast collection of recipes with a user-friendly search feature.
+Recipe Details: Dive into the details of each recipe, including ingredients and step-by-step instructions.
+Category Filters: Browse recipes by categories such as cuisine, dietary restrictions, and more.
+Save Favorites: Mark your favorite recipes for quick access in the future.
+Interactive UI: Enjoy a smooth and responsive user interface with CSS animations and transitions.
+Tools and Technologies
+React: Building the application's dynamic UI and components.
+React Router: Managing navigation and routing within the application.
+Tailwind CSS: Styling the application for a modern and responsive design.
+Data Fetching: Retrieving recipe data from external APIs.
+CSS Animation: Enhancing the user experience with subtle animations.
+
+
+
+To run the application RecipeFinder, follow these steps:-
+
+Install dependencies: npm install
+
+Start the development server:  npm start
+
+For production Deployment : npm run build
+
+
+
+Conclusion :-
+
+RecipeFinder demonstrates the power of modern web technologies to create a delightful recipe discovery experience.
+Whether you're a seasoned chef or a kitchen novice, Foodverse invites you to explore, create, and savor culinary delights like never before. 
+Dive into the world of flavors and start cooking up your favorites today!
+
+Screenshots :-
+
+![image](https://github.com/ABHINEET4/RecipeFinder-Application/assets/108821830/e155a047-174b-4c27-924a-fdfd6d5c67da)
+
+
+![image](https://github.com/ABHINEET4/RecipeFinder-Application/assets/108821830/5a155a66-ad5b-48ba-b19d-99c9f07026a7)
+
+
+
+![image](https://github.com/ABHINEET4/RecipeFinder-Application/assets/108821830/119ad374-d94a-485d-8494-6032baabb641)
+
+
+
+
+![image](https://github.com/ABHINEET4/RecipeFinder-Application/assets/108821830/e47f480a-cf2e-40c5-b37f-5c05fb3a0689)
+
+
+
+
